f0f379226ef87c93356d5b5b106b056505dd301f
Håkon Guttulsrud <48434488+Guttulsrud@users.noreply.github.com>
2020-04-23 12:06:43 +0200

    Add files via upload

--- Embedded.ino
+++ Embedded.ino
@@ -1,91 +1,89 @@
 
- #include "Particle.h"
- #include "dht11.h"
+#include "Adafruit_DHT_Particle.h"
+#include "Particle.h"
 
- dht11 DHT11;
+#define DHTPIN 5
 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
- void hookResponseHandler(const char *event, const char *data);
+void hookResponseHandler(const char *event, const char *data);
 
+int stepperUp = D7;
+int stepperDown = D6;
+float humidity = 0;
+float temperature = 0;
+int light = 0;
+String dhtData = String(0);
+int lightSensorPin = A0;
+#include "TimeAlarms/TimeAlarms.h"
 
- int stepperUp = D7;
- int stepperDown = D6;
- int humidity = 0;
- int temperature = 0;
- int light = 0;
- String dhtData = String(0);
- int lightSensorPin = A0;
+void setup() {
 
- void setup() {
+  Serial.begin(9600);
+  dht.begin();
 
-   Serial.begin(9600);
-   pinMode(stepperUp, OUTPUT);
-   pinMode(stepperDown, OUTPUT);
-   digitalWrite(stepperDown, LOW);
-   digitalWrite(stepperUp, LOW);
+  pinMode(stepperUp, OUTPUT);
+  pinMode(stepperDown, OUTPUT);
+  digitalWrite(stepperDown, LOW);
+  digitalWrite(stepperUp, LOW);
 
-  //  Particle.subscribe("hook-response/weather", hookResponseHandler, MY_DEVICES);
-  //  takeReading();
+  //  Particle.subscribe("hook-response/weather", hookResponseHandler,
+  //  MY_DEVICES); takeReading();
+  Time.zone(+1);
+  Serial.begin(9600);
 
+  delay(3000);
 
- }
+  for (int i = 0; i < 25; i++) {
 
+    Alarm.alarmRepeat(i, 00, 0, EveningAlarm); // 5:45pm every day
+  }
+}
 
- void loop() {
+void loop() {
 
-     takeReading();
-     delay(360000);
- }
+  Alarm.delay(1000);
 
+  delay(2000);
+  // takeReading();
+  // delay(15000);
+}
 
- void hookResponseHandler(const char *event, const char *data) {
+void EveningAlarm() { takeReading(); }
+
+void hookResponseHandler(const char *event, const char *data) {
   //  String data2 = "{\"field3\":"+ String(data)+"}";
   //  Particle.publish("pressure", data, PRIVATE);
   //  Serial.println(data);
- }
-
- // function to take reading from DHT11
-void takeReading() {
-    // get initial reading to check status of board
-    int chk = DHT11.read(2);
-
-    // Serial.println("DEBUG Read sensor: ");
-    // switch (chk)
-    // {
-    // case DHTLIB_OK:
-    //     Particle.publish("DEBUG", "OK");
-    // 	break;
-    // case DHTLIB_ERROR_CHECKSUM:
-    //     Particle.publish("DEBUG", "Checksum error");
-    // 	break;
-    // case DHTLIB_ERROR_TIMEOUT:
-    // 	Particle.publish("DEBUG", "Time out error");
-    // 	break;
-    // default:
-    // 	Particle.publish("DEBUG", "Unknown error");
-    // 	break;
-    // }
-    //
-    //
-
-    // Light level measurement
-    float light_measurement = analogRead(lightSensorPin);
-    light = (int)(light_measurement/4096*100);
-
-
-    temperature = DHT11.temperature;
-    humidity = DHT11.humidity;
-    dhtData = "{\"field1\":"+ String(temperature)+",\"field2\":"+String(humidity)+",\"field3\":"+String(light)+"}";
-
-
-    // Particle.publish("temp", data, PRIVATE);
-    Serial.println("sending!");
-    publishToWebHook("temp", dhtData);
 }
 
-void publishToWebHook(const char *webhook, const char* data) {
-  Particle.publish(webhook, data, PRIVATE);
+// function to take reading from DHT11
+void takeReading() {
+  float h = dht.getHumidity();
+  float t = dht.getTempCelcius();
 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  } else {
+    temperature = t;
+    humidity = h;
+  }
+
+  // todo: move out of this function.
+  float light_measurement = analogRead(lightSensorPin);
+  light = (int)(light_measurement / 4096 * 100);
+
+  dhtData = "{\"field1\":" + String(temperature) +
+            ",\"field2\":" + String(humidity) + ",\"field3\":" + String(light) +
+            "}";
+
+  publishToWebHook("temp", dhtData);
+}
+
+void publishToWebHook(const char *webhook, const char *data) {
+  Particle.publish(webhook, data, PRIVATE);
 }
 
 void activateStepper(int direction) {
@@ -94,11 +92,10 @@ void activateStepper(int direction) {
   digitalWrite(direction, LOW);
 }
 
+void sendSMS(const char *message) {
+  Particle.publish("Twilio", message, PRIVATE);
+}
 
- void sendSMS(const char* message) {
-   Particle.publish("Twilio", message, PRIVATE);
- }
-
- void postTwitterStatus(const char* message) {
-   Particle.publish("Twitter_Status", message, 60, PRIVATE);
- }
+void postTwitterStatus(const char *message) {
+  Particle.publish("Twitter_Status", message, 60, PRIVATE);
+}
